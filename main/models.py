e6876a7e17d39bf56c59871d8da45d34862cbb4d
io-moon00 <sevi97.wenger@gmail.com>
2023-05-19 23:28:31 +0200

    change section model

--- main/models.py
+++ main/models.py
@@ -23,13 +23,20 @@ class logo(models.Model):
         return self.title
 
 class section(models.Model):
-    title = models.CharField(max_length=50)
+    MAIN_SECTIONS = [
+        ("about", "About", ),
+        ("mining", "Mining & Geology", ),
+        ("polar", "Polar Expeditions", ),
+        ("photo", "Photography", ),
+        ("translations", "Translations", ),
+    ]
+    title = models.CharField(choices = MAIN_SECTIONS, max_length=50)
     img = models.ImageField(upload_to='uploads', blank=True, null=True)
     img_alt = models.CharField(max_length=50, blank=True, null=True)
     text = models.TextField()
     text_de = models.TextField(blank=True)
-    sub_sections = models.ForeignKey(sub_section, on_delete=models.CASCADE, blank=True, null=True)   
-    logos = models.ForeignKey(logo, on_delete=models.CASCADE, blank=True, null=True)
+    sub_sections = models.ManyToManyField(sub_section, blank=True, null=True)   
+    logos = models.ManyToManyField(logo, blank=True, null=True)
 
     def __str__(self):
         return self.title
