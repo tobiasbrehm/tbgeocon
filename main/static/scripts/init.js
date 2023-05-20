f12d87569b27833227d61f76d4eafbd6667ecc39
io-moon00 <sevi97.wenger@gmail.com>
2023-05-20 11:35:42 +0200

    fixed slider animation

--- main/static/scripts/init.js
+++ main/static/scripts/init.js
@@ -1,12 +1,51 @@
+function init(){
+
+}
+
+
 
 function openTap(tab){
-    console.log('test')
+    checkedButtonStyle(tab);
+    animateSlider(tab);
+}
+
+function remove_slider_classes(){
+    document.getElementById('slider').classList.remove('slider-one');
+    document.getElementById('slider').classList.remove('slider-two');
+    document.getElementById('slider').classList.remove('slider-three');
+    document.getElementById('slider').classList.remove('slider-four');
+}
+
+function animateSlider(tab){
+    remove_slider_classes();
+    switch (tab) {
+        case 'mining':
+            document.getElementById('slider').classList.add('slider-one');
+            break;
+
+        case 'polar':
+            document.getElementById('slider').classList.add('slider-two');
+            break; 
+            
+        case 'photo':
+            document.getElementById('slider').classList.add('slider-three');
+            break;  
+
+        case 'translations':
+            document.getElementById('slider').classList.add('slider-four'); 
+            break;
+
+        default:
+            break;
+    }
+}
+
+
+function checkedButtonStyle(tab){
     var i;
     let tabs = document.getElementsByClassName("tab");
     for (i = 0; i < tabs.length; i++) {
         tabs[i].classList.remove('checked');
     }
-
     document.getElementById(tab).classList.add('checked');
-
 }
