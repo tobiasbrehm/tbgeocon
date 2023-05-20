08ae5557935a857556e573be808601b8e4d491e6
io-moon00 <sevi97.wenger@gmail.com>
2023-05-21 01:10:49 +0200

    fixed header nav for firefox and safari

--- tbgeocon/settings.py
+++ tbgeocon/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '192.168.0.44']
 
 
 # Application definition
